6317d58f1517da6189e95b8f505534d05974eeb2
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-11-26 08:47:23 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { application } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    response.status(200).send('Hello World');
+});
+
+app.listen(3333, () => console.log(`Rodando na porta 3333`));
