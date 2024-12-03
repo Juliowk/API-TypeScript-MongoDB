bbbed16433f9fab8ec8b31b6cb7828580d6ad8ed
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-12-03 08:43:04 -0300

    feat: add DeleteUser controller adn its routes

--- src/controllers/deleteUsers/deleteUser.ts
+++ src/controllers/deleteUsers/deleteUser.ts
@@ -1,1 +1,36 @@
 // controller
+
+import { User } from "../../models/user.js";
+import { HttpRequest, HttpResponse } from "../protocols.js";
+import { IDeleteUserController, IDeleteUserRepository } from "./protocol.js";
+
+export class DeleteUserController implements IDeleteUserController {
+  constructor(private readonly repository: IDeleteUserRepository) {}
+
+  async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> {
+      
+      try {
+        const id = httpRequest?.params?.id;
+
+        if (!id) {
+            return {
+              statusCode: 400,
+              body: "Id not informed",
+            };
+        }
+
+        const user = await this.repository.deleteUser(id);
+
+        return {
+          statusCode: 200,
+          body: user,
+        };
+        
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: "Something went wrong"
+        }
+    }
+  }
+}
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -6,6 +6,8 @@ import { MongoCreateUser } from "../repositories/createUsers/mongoCreateusers.js
 import { CreateUserController } from "../controllers/createUsers/createUsers.js";
 import { MongoUpdateUsers } from "../repositories/updateUsers/mongoUpdateUsers.js";
 import { UpdateUserController } from "../controllers/updateUsers/updateUsers.js";
+import { MongoDeleteUser } from "../repositories/deleteUsers/deleteUsers.js";
+import { DeleteUserController } from "../controllers/deleteUsers/deleteUser.js";
 
 const route = Router();
 
@@ -30,6 +32,14 @@ route.patch("/user/:id", async (request, response) => {
   const repository = new MongoUpdateUsers();
   const controller = new UpdateUserController(repository);
   const { statusCode, body } = await controller.handle(request);
+  response.status(statusCode).send(body);
+});
+
+route.delete("/user/:id", async (request, response) => {
+  const repository = new MongoDeleteUser();
+  const controller = new DeleteUserController(repository);
+  const { statusCode, body } = await controller.handle(request);
+
   response.status(statusCode).send(body);
 });
 
